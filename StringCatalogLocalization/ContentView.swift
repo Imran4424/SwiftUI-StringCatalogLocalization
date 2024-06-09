0b48c2ad820ad6ff7e87e70aad0c89d81a562df9
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2024-06-09 12:44:07 +0600

    Localized string done

--- StringCatalogLocalization/ContentView.swift
+++ StringCatalogLocalization/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var name = "Dear"
+    @State private var name: LocalizedStringResource = "Dear"
     @State private var loveCounter = 3000
     
     var body: some View {
